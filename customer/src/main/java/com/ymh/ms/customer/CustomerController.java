1e65fdc3489616c6dad3da69cca7a573450993cd
YUN <yunmyeonghun@gmail.com>
2021-12-20 22:37:37 +0900

    study 04

--- customer/src/main/java/com/ymh/ms/customer/CustomerController.java
+++ customer/src/main/java/com/ymh/ms/customer/CustomerController.java
@@ -9,10 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
-public record CustomerController {
+public record CustomerController(CustomerService customerService) {
 
     @PostMapping
-    public void registerCustomer(@RequestBody CustomerRequest customerRequest) {
-        log.info("new customer registration {}", customerRequest);
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
+        log.info("new customer registration {}", customerRegistrationRequest);
+        customerService.registerCustomer(customerRegistrationRequest);
     }
 }
