dbd195f473e5ebbe9a55edb77bcf367bd5586351
YUN <yunmyeonghun@gmail.com>
2022-01-12 11:09:48 +0900

    study 16

--- fraud/src/main/java/com/ymh/ms/fraud/FraudController.java
+++ fraud/src/main/java/com/ymh/ms/fraud/FraudController.java
@@ -1,14 +1,16 @@
 package com.ymh.ms.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@AllArgsConstructor
 @RestController
 @RequestMapping("api/v1/fraud-check")
-@AllArgsConstructor
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
@@ -16,6 +18,7 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraud(@PathVariable("customerId") Integer customerId) {
         boolean isFraudCustomer = fraudCheckService.isFraudCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudCustomer);
     }
 }
