3ae8fad3933102711ecf865b5e96ced099903161
YUN <yunmyeonghun@gmail.com>
2022-01-24 14:28:15 +0900

    study 23 AMQP Config (exchange, queue, routing-key)

--- amqp/src/main/java/com/ymh/ms/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/com/ymh/ms/amqp/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package com.ymh.ms.amqp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -17,9 +18,19 @@ public class RabbitMQConfig {
 
     @Bean
     public AmqpTemplate amqpTemplate() {
+        // Send msg
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter());
         return rabbitTemplate;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+        // Receive msg
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter());
+        return factory;
     }
 
     @Bean
