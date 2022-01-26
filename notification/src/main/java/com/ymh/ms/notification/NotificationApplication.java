e12dd3dec8c3373e779f7fef96295b1885175c72
YUN <yunmyeonghun@gmail.com>
2022-01-26 17:32:01 +0900

    study 25 AMQP send msg test

--- notification/src/main/java/com/ymh/ms/notification/NotificationApplication.java
+++ notification/src/main/java/com/ymh/ms/notification/NotificationApplication.java
@@ -1,15 +1,36 @@
 package com.ymh.ms.notification;
 
+import com.ymh.ms.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients("com.ymh.ms.clients")
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(
+        // For inject bean in CommandLineRunner
+        scanBasePackages = {
+                "com.ymh.ms.amqp",
+                "com.ymh.ms.notification"
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
+    }
+
+    // For AMQP testing
+    // Inject producer & config
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig config) {
+        return args -> {
+            producer.publish(
+                    "YMH is winner",
+                    config.getInternalExchange(),
+                    config.getInternalNotificationRoutingKey());
+        };
     }
 }
