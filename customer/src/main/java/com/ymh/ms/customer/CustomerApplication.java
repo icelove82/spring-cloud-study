861875a59a6ac9a478a69ba71227ef20dbcdd2d6
YUN <yunmyeonghun@gmail.com>
2022-02-12 15:30:40 +0900

    study 36 Refactor for k8s

--- customer/src/main/java/com/ymh/ms/customer/CustomerApplication.java
+++ customer/src/main/java/com/ymh/ms/customer/CustomerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
 
 @EnableFeignClients("com.ymh.ms.clients")
 @EnableEurekaClient
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
                 "com.ymh.ms.customer"
         }
 )
+@PropertySource("classpath:clients-${spring.profiles.active}.properties")
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
--- notification/src/main/java/com/ymh/ms/notification/NotificationApplication.java
+++ notification/src/main/java/com/ymh/ms/notification/NotificationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 @EnableFeignClients("com.ymh.ms.clients")
 @EnableEurekaClient
@@ -17,6 +18,7 @@ import org.springframework.context.annotation.Bean;
                 "com.ymh.ms.notification"
         }
 )
+@PropertySource("classpath:clients-${spring.profiles.active}.properties")
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
